f5d69a6d1357a2fa8119bedfdaf1bda924dc91b9
MasterRahul01 <165376310+MasterRahul01@users.noreply.github.com>
2025-11-08 14:51:17 +0530

    new

---
+++ prg1.py
@@ -0,0 +1,32 @@
+# print("hello version")
+# a=22
+# lang ="hddndjdb"
+# print(a)
+# print(lang)
+
+# cars = ["subaru", "audi", "bmw", "toyota"]
+# print(cars)
+
+# cars.append("mercedes")
+# for car in cars:0.
+#     print(car)
+
+# i = 10
+# for i in range(0,5):
+#     print(i)
+
+# while i < 10:
+#     print(i)
+#     i += 1
+
+# nums = [1,2,3,4,5]
+# max_num = max(nums)
+# print( "max is :" + str(max_num))
+
+# min_num = min(nums)
+# print("min is :" + str(min_num))
+
+def greet(name):
+    return "Hello, " + name + "!"
+def greet():
+    return "Hello, World!"
